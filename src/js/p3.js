ffc54aa549f2f470c67be8b7df9e568652b43f4b
twinogre <glad2you@gmail.com>
2024-12-09 16:56:52 +0900

    p2 페이지네이션 제외 완성

---
+++ src/js/p3.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+  // p3 s3 스와이퍼
+  var s3swiper = new Swiper(".s3-swiper", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
