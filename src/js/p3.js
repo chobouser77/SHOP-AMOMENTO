33dc469405d4b8bd04b705a3c0aac27e80a999ad
twinogre <glad2you@gmail.com>
2025-01-02 18:41:23 +0900

    p3 반응형 구현

--- src/js/p3.js
+++ src/js/p3.js
@@ -2,11 +2,16 @@
 $(document).ready(function() {
   // p3 s3 스와이퍼
   var s3swiper = new Swiper(".s3-swiper", {
-    slidesPerView: 6,
+    slidesPerView: 2, // 기본 슬라이드 수
     spaceBetween: 20,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+    breakpoints: {
+      768: {
+        slidesPerView: 6, 
+      },
+    },
+  });  
 });
